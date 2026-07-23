7ec723615a79a0e4a04f40dd2ebaa911c5ab5d93
HavGraphic <havgraphic.co@gmail.com>
2026-07-23 17:22:03 -0400

    Corriger les chemins d'icônes du manifest

--- client/app/manifest.js
+++ client/app/manifest.js
@@ -8,8 +8,8 @@ export default function manifest() {
     background_color: '#24292E',
     theme_color: '#24292E',
     icons: [
-      { src: '/icon', sizes: '32x32', type: 'image/png' },
-      { src: '/apple-icon', sizes: '180x180', type: 'image/png' },
+      { src: '/icon.png', sizes: '512x512', type: 'image/png' },
+      { src: '/apple-icon.png', sizes: '180x180', type: 'image/png' },
     ],
   };
 }
